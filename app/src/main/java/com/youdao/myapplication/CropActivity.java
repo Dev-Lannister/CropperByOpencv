4e700503d1199e66c35536435463500ee34a9c71
LiJiWei <lijiwei@rd.netease.com>
2020-03-06 16:41:16 +0800

    feat:客制化截取框

--- app/src/main/java/com/youdao/myapplication/CropActivity.java
+++ app/src/main/java/com/youdao/myapplication/CropActivity.java
@@ -14,6 +14,7 @@ import android.provider.MediaStore;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -56,6 +57,7 @@ public class CropActivity extends AppCompatActivity implements EasyPermissions.P
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_crop);
         ivCrop = (CropImageView) findViewById(R.id.iv_crop);
         btnCancel = (Button) findViewById(R.id.btn_cancel);
